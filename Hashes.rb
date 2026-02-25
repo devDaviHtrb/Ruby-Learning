b759042c9f27cc990d2a079d3a8476aceb4ceb60
devDaviHtrb <davideleteribeiro1@gmail.com>
2026-02-25 10:45:04 -0300

    <add> hash methods

--- Hashes.rb
+++ Hashes.rb
@@ -5,3 +5,15 @@ opcoes = { "cor" => "azul", 1 => "primeiro" }
 
 puts usuario[:nome]
 puts opcoes["cor"]
+
+usuario.key?(:nome) #verifica se a chave existe
+usuario.fecth(:casa, "não existe") # verifica se uma chave exise e retorna um valor padrão se não existir
+
+
+precos = { cafe: 5, pao: 2, bolo: 15 }
+precos.select { |chave, valor| valor < 10 }
+
+precos.keys #faz um array temporario com as chaves
+precos.values #faz um array temporario com os valores
+
+precos.transform_values { |value| value * 2 } # Dobra todos os preços

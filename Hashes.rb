ab666d30e4ca4473d94e96a741af157e7dd2ad59
devDaviHtrb <davideleteribeiro1@gmail.com>
2026-02-25 10:36:57 -0300

    <add> hashes

---
+++ Hashes.rb
@@ -0,0 +1,7 @@
+usuario = { nome: "Alice", idade: 25 }
+opcoes = { "cor" => "azul", 1 => "primeiro" }
+
+#se a chave n for um simbolo (string imutavel) tem que usar uma arrow
+
+puts usuario[:nome]
+puts opcoes["cor"]
